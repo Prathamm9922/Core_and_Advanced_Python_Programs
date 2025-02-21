431ba099035e0000dac209da808cd728d55c7049
Prathamm9922 <prathamjadhav0830@gmail.com>
2025-02-21 12:36:45 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,20 @@
+# Single line comment: program to perform arithmetic operation
+# Enter the First Number
+num1 = float(input("Enter the first number "))
+# Enter the Second Number
+num2 = float(input("Enter the second number "))
+# write The function
+add = num1 + num2
+mult = num1 * num2
+# Display The Statement
+print("Addition:", add)
+print("Multiplication:", mult)
+
+
+
+''' Output
+Enter the first number 15
+Enter the second number 25
+Addition: 40.0
+Multiplication: 375.0
+'''
